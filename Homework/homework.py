1541ec410366f841622d4a74b7bb661d5c6b57dc
sokolaleksej <alsok@jet.msk.su>
2017-05-03 15:16:41 +0300

    test

--- Homework/homework.py
+++ Homework/homework.py
@@ -1,13 +1,22 @@
 import re
 import glob
+import ipaddress
 
 
+spisok1 = []
+IP1 = []
+gw = []
 
 def Classif (s):
 
     IP = re.match("^ ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", s)
     if IP:
-        return ("IP", IP.group(1), IP.group(2))
+        return IP1.append((ipaddress.IPv4Network((IP.group(1), IP.group(2)), strict=False)))
+
+    Gate = re.match('ip default-gateway (.*)', s)
+    if Gate:
+        return gw.append(ipaddress.IPv4Address(Gate.group(1)))
+
 
 
 
@@ -17,6 +26,18 @@ for i in n:
   lines = f.readlines()
   f.close()
   for j in lines:
-        spisok = Classif(j)
+        Classif(j)
 
-        print (spisok)
+
+for t in (set(IP1)):
+    print (t)
+for r in (set(gw)):
+    print (r)
+#        spisok1.append(spisok)
+#        spisok1 = ' '.join('spisok')
+
+#print (set(spisok1))
+
+
+#for t in (set(spisok1)):
+#    print (t)
